c6245c2c2ee041e08e38219ea527ee0fa26edb58
kcastanoa <kcastanoa@unal.edu.co>
2025-09-26 15:25:44 -0500

    prueba

--- homework/queries.py
+++ homework/queries.py
@@ -3,6 +3,93 @@
 # pylint: disable=broad-exception-raised
 # pylint: disable=import-error
 
+from homework.mapreduce import hadoop
+
+
+#
+# Columns:
+# total_bill, tip, sex, smoker, day, time, size
+#
+
+#
+# CONSULTA 1:
+# SELECT *, tip/total_bill as tip_rate
+# FROM tips;
+#
+def mapper_query_1(sequence):
+    """Mapper"""
+    result = []
+    for index, (_, row) in enumerate(sequence):
+        if index == 0:
+            result.append( 
+                (
+                    index, 
+                    row.strip() + ',tip_rate'
+                )  
+            )
+        else:
+            row_values = row.strip().split(",")
+            total_bill = float(row_values[0])
+            tip = float(row_values[1])            
+            tip_rate = tip / total_bill
+            result.append(
+                (index, row.strip() + "," + str(tip_rate))
+            )
+
+    return result
+
+def reducer_query_1(sequence):
+    return sequence
+
+
+
+#
+# SELECT *
+# FROM tips
+# WHERE time = 'Dinner';
+#
+def mapper_query_2(sequence):
+    """Mapper"""
+    result = []
+    for index, (_, row) in enumerate(sequence):
+        if index == 0:
+            result.append((index, row.strip()))
+        else:
+            row_values = row.strip().split(",")
+            if row_values[5] == "Dinner":
+                result.append((index, row.strip()))
+    return result
+
+
+def reducer_query_2(sequence):
+    """Reducer"""
+    return sequence
+
+
+
+#
+# SELECT *
+# FROM tips
+# WHERE time = 'Dinner' AND tip > 5.00;
+#
+def mapper_query_3(sequence):
+    """Mapper"""
+    result = []
+    for index, (_, row) in enumerate(sequence):
+        if index == 0:
+            result.append((index, row.strip()))
+        else:
+            row_values = row.strip().split(",")
+            if row_values[5] == "Dinner" and float(row_values[1]) > 5.00:
+                result.append((index, row.strip()))
+    return result
+
+
+def reducer_query_3(sequence):
+    """Reducer"""
+    return sequence
+
+
 
 #
 # ORQUESTADOR:
@@ -11,8 +98,27 @@ def run():
     """Orquestador"""
 
 
+    hadoop(
+        mapper_fn=mapper_query_1,
+        reducer_fn=reducer_query_1,
+        input_folder="files/input",
+        output_folder="files/query_1",
+    )
+
+    hadoop(
+        mapper_fn=mapper_query_2,
+        reducer_fn=reducer_query_2,
+        input_folder="files/input",
+        output_folder="files/query_2",
+    )    
+
+    hadoop(
+        mapper_fn=mapper_query_3,
+        reducer_fn=reducer_query_3,
+        input_folder="files/input",
+        output_folder="files/query_3",
+    )        
+
 if __name__ == "__main__":
 
     run()
-
-
